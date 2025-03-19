4039f2d6d52056eafbe11aefd72a5a45dec837b0
joaofelpe14 <jaofelpe1@gmail.com>
2025-03-19 13:29:15 -0300

    feat: :art: Configurando rotas componentes e pages do projeto
    Configurando rotas componentes e pages do projeto
    
    Configurando rotas componentes e pages do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FavoritosComponent } from './pages/favoritos/favoritos.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'home', component: HomeComponent }, 
+  { path: 'favoritos', component: FavoritosComponent }, 
+  { path: 'sobre', component: SobreComponent },
+  { path: '**', redirectTo: '' } 
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FavoritosComponent } from './pages/favoritos/favoritos.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { ModalDetalhesComponent } from './components/modal-detalhes/modal-detalhes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FavoritosComponent,
+    SobreComponent,
+    RodapeComponent,
+    CabecalhoComponent,
+    ModalDetalhesComponent
   ],
   imports: [
     BrowserModule,
